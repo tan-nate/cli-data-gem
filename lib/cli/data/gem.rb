74d77e1f33ad7728580922c304bca5444c345a08
Nate Tan <nktan93@gmail.com>
2019-04-22 20:27:00 +0000

    first commit

---
+++ lib/cli/data/gem.rb
@@ -0,0 +1,10 @@
+require "cli/data/gem/version"
+
+module Cli
+  module Data
+    module Gem
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/cli/data/gem/version.rb
@@ -0,0 +1,7 @@
+module Cli
+  module Data
+    module Gem
+      VERSION = "0.1.0"
+    end
+  end
+end
