778f20df5d4522125d69f54e9c9d457e35822ef1
Nate Tan <nktan93@gmail.com>
2019-04-26 20:02:29 +0000

    worked on beer.rb

--- lib/cli/data/gem/beer.rb
+++ lib/cli/data/gem/beer.rb
@@ -9,5 +9,22 @@ class Beer
   
   @@all = []
   
+  def initialize(beer_hash)
+    beer_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
+  end
   
+  def self.create_from_collection(beers_array)
+    beers_array.each do |beer_hash|
+      Beer.new(beer_hash)
+    end
+  end
+  
+  def self.all
+    @@all
+  end
 end
+
+Beer.create_from_collection(Scraper.scrape_list_page)
+
+binding.pry
