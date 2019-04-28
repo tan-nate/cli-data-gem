3a6ac878e56afff3ef97319c436e735bc4f89b9c
Nate Tan <nktan93@gmail.com>
2019-04-28 06:15:21 +0000

    setup

--- config/environment.rb
+++
@@ -1,9 +0,0 @@
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
-
-require_relative '../lib/beer_advocate/beer'
-require_relative '../lib/beer_advocate/brewery'
-require_relative '../lib/beer_advocate/scraper'
-require_relative '../lib/beer_advocate/style'
-require_relative '../lib/beer_advocate/version'
--- lib/beer_advocate.rb
+++ lib/beer_advocate.rb
@@ -1,1 +1,9 @@
-require_relative '../config/environment'
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/beer_advocate/beer'
+require_relative '../lib/beer_advocate/brewery'
+require_relative '../lib/beer_advocate/scraper'
+require_relative '../lib/beer_advocate/style'
+require_relative '../lib/beer_advocate/version'
