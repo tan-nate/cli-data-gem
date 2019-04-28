7dcbe1641ba183b6455e35a8fd6476daf1658235
Nate Tan <nktan93@gmail.com>
2019-04-28 20:16:51 +0000

    worked on CLI

--- lib/beer_advocate/cli.rb
+++ lib/beer_advocate/cli.rb
@@ -1,5 +1,7 @@
 class BeerAdvocate::CLI
-  def call
-    puts "Hello!"
+  def welcome
+    puts "Welcome to the Beer Advocate CLI! At any time, type 'menu' to return to this menu or 'back' to return to the previous page."
+    puts " 1. Browse by beer  | 2. Search by beer"
+    puts " 2. Browse by style | 4. Search by brewery"
   end
 end
