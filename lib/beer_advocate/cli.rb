d680e97d4eea8663c59de7db169d4d96cee2348f
Nate Tan <nktan93@gmail.com>
2019-04-28 22:54:31 +0000

    finished draft of show_style(style) in cli.rb

--- lib/beer_advocate/cli.rb
+++ lib/beer_advocate/cli.rb
@@ -20,6 +20,21 @@ class BeerAdvocate::CLI
     end
   end
   
+  def show_beer(beer)
+    beers = BeerAdvocate::Beer.create_from_collection(BeerAdvocate::Scraper.scrape_list_page)
+    find_beer = beers.find do |listed_beer|
+      listed_beer[:name].downcase == beer.downcase
+    end
+    beer_page_details = BeerAdvocate::Scraper.scrape_name_page(find_beer[:name_url])
+    
+    puts "#{find_beer[:name]}"
+    puts "Brewery: #{find_beer[:brewery]} | Review count: #{find_beer[:review_count]}"
+    puts "Style: #{find_beer[:style]} | Score: #{find_beer[:score]}"
+    puts "ABV: #{find_beer[:abv]}"
+    puts "1. Top reviews | 2. Brewery details | 3. Style details"
+    puts "Choose an option above, or type 'back' or 'menu'"
+  end
+  
   def show_styles_list
     beers = BeerAdvocate::Beer.create_from_collection(BeerAdvocate::Scraper.scrape_list_page)
     styles = beers.collect do |beer|
@@ -27,22 +42,24 @@ class BeerAdvocate::CLI
     end
     styles.uniq!
     styles.sort!
+    
     styles.each do |style|
       puts "#{styles.index(style)}. #{style}"
     end
   end
   
-  def show_beer(beer)
+  def show_style(style)
     beers = BeerAdvocate::Beer.create_from_collection(BeerAdvocate::Scraper.scrape_list_page)
-    find_beer = beers.find do |listed_beer|
-      listed_beer[:name].downcase == beer.downcase
+    find_style = beers.find do |listed_beer|
+      listed_beer[:style].downcase == style.downcase
     end
-    beer_page_details = BeerAdvocate::Scraper.scrape_name_page(find_beer[:name_url])
-    puts "#{find_beer[:name]}"
-    puts "Brewery: #{find_beer[:brewery]} | Review count: #{find_beer[:review_count]}"
-    puts "Style: #{find_beer[:style]} | Score: #{find_beer[:score]}"
-    puts "ABV: #{find_beer[:abv]}"
-    puts "1. Top reviews | 2. Brewery details | 3. Style details"
-    puts "Choose an option above, or type 'back' or 'menu'"
+    style_page_details = BeerAdvocate::Scraper.scrape_style_page(find_style[:style_url])
+
+    puts "#{find_style[:style]}"
+    puts "Description: #{style_page_details[:description]}"
+    puts "#{style_page_details[:abv]}"
+    puts "#{style_page_details[:ibu]}"
+    puts "#{style_page_details[:glassware]}"
+    puts "Press '1' for a list of beers of this style"
   end
 end
